3d2f113830332e10fc9de524a7005da023054891
sgeorge-21 <srangeorge@gmail.com>
2026-06-08 09:36:58 +0000

    Update host photo import in about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import Layout, { ShowSidebar } from "@/components/lbh/Layout";
-import hostPhoto from "@/assets/james-worquea.jpg";
+import hostPhoto from "@/assets/james-t-worquea.jpg";
 
 export const Route = createFileRoute("/about")({ component: AboutPage });
 
